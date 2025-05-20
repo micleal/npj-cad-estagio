2b4d1f088267bf2b89221ed6accd157b7524b4e2
Michael A L Costa <github@micleal.dev>
2025-05-20 19:38:52 -0300

    fix: corrige erro de URL do better-auth.

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -8,6 +8,7 @@ import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   appName: "Cadastro de atendimentos NPJ",
+  baseURL: env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
